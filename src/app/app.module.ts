f28d8cf7d2698230ba898f202e2022562c28baf2
emegea <martosproject@gmail.com>
2022-02-26 18:59:40 -0300

    primer commit, subiendo todo

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { QuiensoyComponent } from './componentes/quiensoy/quiensoy.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { SocialComponent } from './componentes/social/social.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    QuiensoyComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    HabilidadesComponent,
+    FooterComponent,
+    SocialComponent,
+    ExperienciaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/componentes/quiensoy/quiensoy.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiensoy',
+  templateUrl: './quiensoy.component.html',
+  styleUrls: ['./quiensoy.component.css']
+})
+export class QuiensoyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  saludo() {
+    console.log('hola');
+  }
+}
